d3ab7ffc67516dbadbe74872de14a6079d0a8419
Marco Lanaro <marcolanaro@gmail.com>
2012-02-18 02:30:37 -0800

    Grid worker all-in-one

--- src/dijkstra-grid.js
+++ src/dijkstra-grid.js
@@ -1,5 +1,4 @@
 var L1, L2, obj, graph = [];
-var worker = new Worker('dijkstra.js');
 
 self.onmessage = function(event) {
 	self.postMessage({prova:1});
@@ -47,19 +46,17 @@ self.onmessage = function(event) {
 			}
 		}
 	}
-	self.postMessage({prova:2});
 
-	worker.onmessage = function(e) {
-		var v = new Array();
-		for (var i = 0; i < e.data.length; i += 1)
-			v[i] = Vec2Grid(e.data[i]);
-		self.postMessage(v);
-	};  
-	worker.postMessage({
+	var m = dijkstra({
 		from: Grid2Vec(obj.from),
 		to: Grid2Vec(obj.to),
 		graph: graph
 	});
+
+	var v = new Array();
+	for (var i = 0; i < m.length; i += 1)
+		v[i] = Vec2Grid(m[i]);
+	self.postMessage(v);
 }
 
 append = function(n, c, w) {
@@ -80,4 +77,83 @@ Vec2Grid = function(n) {
 
 Grid2Vec = function(c) {
 	return c.x + c.y * L1 + c.z * L1 * L2;			
+}
+
+dijkstra = function(obj) {
+	var V = {
+			weight: [],
+			found: [],
+			father: []
+		};
+		
+	// initialization
+	for (var i = 0; i < obj.graph.length; i += 1) {
+		V.weight[i] = 10000000;
+		V.found[i] = false;
+		V.father[i] = -1;
+	}
+	
+	// i know where i am
+	V.found[obj.from] = true;
+	V.weight[obj.from] = 0;
+	
+	// start the search
+	var found = false;
+	var node = obj.from;
+	var node_old = node;
+	while (!found) {
+		// the most intelligent things to do is look the world around you
+		for (var i = 0; i < obj.graph[node].length; i += 1) {
+			var new_weight = V.weight[node] + obj.graph[node][i].w;
+			if (V.weight[obj.graph[node][i].n] > new_weight) {
+				V.father[obj.graph[node][i].n] = node;
+				V.weight[obj.graph[node][i].n] = new_weight;
+			}
+		}
+		
+		// i want to be light
+		var light = 10000000;
+		var lightest;
+		for (var i = 0; i < obj.graph.length; i += 1) {
+			if (!V.found[i] && V.weight[i] <= light) {
+				light = V.weight[i];
+				lightest = i;
+			}
+		}
+		
+		// oh right, lightest you are safe ;)
+		V.found[lightest] = true;
+		
+		// and now you are the boss!
+		node = lightest;
+		
+		// oops... i iterate the same path: there is no way :(
+		if (node == node_old)
+			found = true;
+		node_old = node;
+		
+		// you are a good monkey
+		if (node == obj.to)
+			found = true
+	}
+	
+	// calculate the path
+	var movement = new Array();
+	if (V.found[obj.to]) {
+		found = false;
+		node = obj.to;
+		while (!found){
+			movement.push(node);
+			node = V.father[node];
+			if (obj.from == node)
+				found = true;
+		}
+		// reverse
+		var message=[];
+		for (var i = movement.length; i > 0; i -= 1) {
+			message.push(movement[i-1]);
+		}
+	}
+		
+	return message;
 }
